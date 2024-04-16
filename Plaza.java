9e1287435b5fbb880c0abc7edb5027e394be98a7
DavidSanchez9 <167196588+DavidSanchez9@users.noreply.github.com>
2024-04-16 13:42:18 +0200

    Añadir contador de reloj

--- Plaza.java
+++ Plaza.java
@@ -1,25 +1,42 @@
 package model;
 
-public class Plaza{
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Plaza {
 	public Coche cocheAsignado;
 	public String id;
-	
-	
-	public Plaza( int id) {
+	private LocalDate horaEntrada;
+	private LocalDate horaSalida;
+
+	public Plaza(String id) {
 		super();
-	
+
 		this.id = id;
 	}
+
 	public void asignarCoche(Coche coche) {
-		this.cocheAsignado=coche;
-		
+		this.cocheAsignado = coche;
+		this.horaEntrada = LocalDate.now();
+
 	}
-	public void eliminarCoche(Coche coche){
+
+	public long eliminarCoche(Coche coche){
+		
 		this.cocheAsignado=null;
+		this.horaSalida=LocalDate.now();
+		return ChronoUnit.HOURS.between(horaEntrada, horaSalida);
+		
 		
 	}
-	
-	
-	
+
+//	public long calcularDuracionEstacionamientoEnHoras() {
+	// if (horaEntrada == null || horaSalida == null) {
+	// throw new IllegalStateException("No se han establecido las marcas de tiempo
+	// de entrada y salida");
+	// }
+
+	// return ChronoUnit.HOURS.between(horaEntrada, horaSalida);
+	   //  }
 
 }
